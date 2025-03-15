331fe3f4de4fe734a6d51383cff3b6c1c7da225c
adhnannp <adhnanusman1234@gmail.com>
2025-03-15 15:13:32 +0530

    feat:backend enhancement

--- server/src/services/auth/OTPService.ts
+++ server/src/services/auth/OTPService.ts
@@ -1,26 +1,26 @@
 // src/services/OTPService.ts
 import { injectable } from 'inversify';
 import { IOTPService } from '../../core/interfaces/services/IOTPService';
-import nodemailer from 'nodemailer';
-import redis from 'redis';
+import { redisClient } from "../../config/redis"
 import { generateOTP, sendOTPEmail } from '../../utils/otp';
 
 @injectable()
 export class OTPService implements IOTPService {
-  private redisClient = redis.createClient();
-
+  constructor() {
+    redisClient.on('error', (err) => console.error('Redis Client Error:', err));
+  }
   generateOTP(): string {
     return generateOTP()
   }
 
   async sendOTP(email: string, otp: string): Promise<void> {
     sendOTPEmail(email,otp)
-    this.redisClient.set(email, otp, {EX:300});
+    await redisClient.set(email, otp, {EX:300});
   }
 
   async verifyOTP(email: string, otp: string): Promise<boolean> {
     try {
-      const storedOTP = await this.redisClient.get(email);
+      const storedOTP = await redisClient.get(email);
       return storedOTP === otp;
     } catch (err) {
       console.error('Error verifying OTP:', err);
